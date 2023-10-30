a1422c7bb29fd45b128f617671ca9c3ddceac872
viniciusfazolaro <vinicius.fazolaro@gmail.com>
2023-10-30 09:46:50 -0300

    commit

---
+++ 1070 - Seis Números Ímpares/Program.cs
@@ -0,0 +1,23 @@
+﻿using System; 
+using System.Globalization;
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int X = int.Parse(Console.ReadLine()), count = 0;
+
+        do{
+
+            if(X % 2 != 0){
+                Console.WriteLine(X);
+                count++;
+            }
+
+            X++;
+            
+        } while(count < 6);
+
+    }
+
+}
